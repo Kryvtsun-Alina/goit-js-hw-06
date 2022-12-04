a0a3cc18b725d59cc31b5c985725c7320fa979af
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-04 16:40:58 +0200

    додає 4 завдання

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,12 @@
 let counterValue = 0;
+const displayValue = document.querySelector('#value');
+const btnMinus = document.querySelector('button[data-action="decrement"]')
+const btnPlus = document.querySelector('button[data-action="increment"]')
+btnMinus.addEventListener('click', () => {
+    counterValue--
+	displayValue.textContent = counterValue;       
+});
+btnPlus.addEventListener('click',  () => {
+    counterValue++
+	displayValue.textContent = counterValue;
+});
