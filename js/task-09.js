d578007ec136bb21bf3cc8b0cdbd1da7c6655bb4
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-04 18:44:55 +0200

    додає завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+};
+const btnChangeColor = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+btnChangeColor.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+});
