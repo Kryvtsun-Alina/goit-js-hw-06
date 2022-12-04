0a7b2862f1322afa018a897fc385dec38362985e
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-04 18:29:26 +0200

    додає 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const Form = document.querySelector('.login-form');
+Form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {elements: { email, password },
+    } = event.currentTarget
 
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    const userInformation = { email: email.value, Password: password.value }
+    
+    console.log(userInformation)
+    event.currentTarget.reset()
+};
