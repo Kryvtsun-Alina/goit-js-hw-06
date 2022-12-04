e5b9b34ebe182d5f6da30a5d3092d487b70c8d97
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-04 21:12:41 +0200

    вносить виправлення до 1 завдання

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,8 @@ const allCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${allCategories.length}`);
 const categoriesArray = [...allCategories]
   .map(
-    categories => `Category: ${categories.children[0].textContent}
-    Elements: ${categories.children[1].children.length}`
+    categories => `Category: ${categories.firstElementChild.textContent}
+    Elements: ${categories.lastElementChild.children.length}`
   )
   .join("\n");
   console.log(categoriesArray);
