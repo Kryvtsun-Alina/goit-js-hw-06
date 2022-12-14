02be0bb8dbf3ab9df6f28790198ce06340d88f0a
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-14 23:14:53 +0200

    Вносить правки

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,6 @@
 const textInputEl = document.querySelector('#name-input');
 let outputEl = document.querySelector('#name-output');
 textInputEl.addEventListener('input', onInputChange);
-// function onInputChange(event) {
-//     inputEl = event.currentTarget.value;
-//     console.log(inputEl);
-  
-//     inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : 'незнакомец';
-//   }
 function onInputChange(event) {
     return event.currentTarget.value
     ? (outputEl.textContent = event.currentTarget.value) 
