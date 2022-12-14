2d1a882f915531915f887b9136e2ab849765d73b
Kryvtsun Alina <krivtsun1002@gmail.com>
2022-12-14 23:11:21 +0200

    Виправляє помилки ДЗ

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,14 @@
 const textInputEl = document.querySelector('#name-input');
-const outputEl = document.querySelector('#name-output');
-textInputEl.addEventListener('input', event => {
-    outputEl.textContent = event.target.value;
-    if(event.target.value === " "){
-        outputEl.textContent = "Anonymous";
-    }
-});
+let outputEl = document.querySelector('#name-output');
+textInputEl.addEventListener('input', onInputChange);
+// function onInputChange(event) {
+//     inputEl = event.currentTarget.value;
+//     console.log(inputEl);
+  
+//     inputEl !== ' ' ? (outputEl.innerText = inputEl.trim()) : 'незнакомец';
+//   }
+function onInputChange(event) {
+    return event.currentTarget.value
+    ? (outputEl.textContent = event.currentTarget.value) 
+    : (outputEl.textContent = "Anonymous");
+};
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,5 @@
 const inputChangeFontesize = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
-inputChangeFontesize.addEventListener('change', event => {
+inputChangeFontesize.addEventListener('input', event => {
     textEl.style.fontSize = `${event.target.value}px`;
 });
